cb0befca98153c925c2adb3a38a0a244f9a7d3ca
prabhat <prabhat@rhinontech.com>
2025-05-21 00:30:01 +0530

    newchanges

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Loading from "./loading";
 
 export default function Home() {
   const router = useRouter();
-  //   const authToken = Cookies.get("authToken");
 
   const redirectAccToUserRole = (userRole: string) => {
     switch (userRole) {
